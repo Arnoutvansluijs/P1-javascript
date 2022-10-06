3811a89ab4bfef90663bc7d5158c1339207b7576
arnoutvansluijs <Arnout261vansluijs@gmail.com>
2022-10-06 13:24:50 +0200

    Opdracht_6.2

---
+++ Opdracht_6.2/app.js
@@ -0,0 +1,8 @@
+function kleur_aanpassen(){
+document.getElementById("kleur_aanpassen").style.color = "red";
+document.getElementById("kleur_aanpassen").innerText = "Ik krijg een tia van hun"
+
+}
+
+
+
