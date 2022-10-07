695eb94f842ca185fa4741d797dda73761bac99a
arnoutvansluijs <Arnout261vansluijs@gmail.com>
2022-10-07 09:32:51 +0200

    x

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,19 @@
+function calculate(x, y) {
+    return x + y;
+}
+
+console.log (calculate(90, 30))
+
+function multiplyByFive (x) {
+    let answer = x * 5;
+ console.log(answer)
+}
+
+multiplyByFive(540)
+
+function calculateMinutes(x) {
+   console.log(x / 60)  ;
+}
+calculateMinutes(3600);
+
+
